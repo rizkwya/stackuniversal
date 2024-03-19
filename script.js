4847a9037d02da8e31a93c4f11e7898c2c9b5a34
Rizk <87561258+rizkwya@users.noreply.github.com>
2024-03-19 23:07:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('x').addEventListener('click', function () {
+    var urlTujuan = 'https://discord.com/invite/8BNhyDZ';
+    window.open(urlTujuan, '_blank');
+});
+
+function menujustore() {
+    var urlTujuan = 'https://www.instagram.com/stackustore';
+    window.open(urlTujuan, '_blank');
+}
