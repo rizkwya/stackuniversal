8664ebcab8570440aa500b78678357ed105a4862
Rizk <87561258+rizkwya@users.noreply.github.com>
2024-06-15 01:06:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('x').addEventListener('click', function () {
-    var urlTujuan = 'https://discord.com/invite/8BNhyDZ';
-    window.open(urlTujuan, '_blank');
-});
-
-function menujustore() {
-    var urlTujuan = 'https://www.instagram.com/stackustore';
-    window.open(urlTujuan, '_blank');
-}
